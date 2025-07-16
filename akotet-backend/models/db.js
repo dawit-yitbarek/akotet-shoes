9437d7234af7106fac25eafe25cdc33baef10812
dawit-yitbarek <dawityit17@gmail.com>
2025-07-16 23:14:02 +0300

    Update db.js 2

--- akotet-backend/models/db.js
+++ akotet-backend/models/db.js
@@ -5,14 +5,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: 'db.egcbirmaycyqorbyzlau.supabase.co',
-  port: 5432,
-  user: 'postgres',
-  password: 'Dawit@#2005',
-  database: 'postgres',
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 export default pool;
