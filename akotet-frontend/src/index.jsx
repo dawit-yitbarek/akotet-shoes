ee1226e3dcfd3c28b6e9b0ed8bc2f1ddb5b0528d
dawit-yitbarek <dawityit17@gmail.com>
2025-11-22 14:25:34 +0300

    Remove unnecessary comments

--- akotet-frontend/src/index.jsx
+++ akotet-frontend/src/index.jsx
@@ -1,17 +1,3 @@
-// import { Buffer } from "buffer";
-// window.Buffer = Buffer;
-// import React, { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//       <StrictMode>
-//             <App />
-//       </StrictMode>
-
-// );
-
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
 
